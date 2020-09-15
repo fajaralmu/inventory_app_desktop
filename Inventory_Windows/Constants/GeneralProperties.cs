3ece49712e9e13e14a18ee71d5cce09adbf92b68
Fajar AM <somabangsa@gmail.com>
2020-09-15 14:17:59 +0700

    add constant

---
+++ Inventory_Windows/Constants/GeneralProperties.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_Windows.Constants
+{
+    class GeneralProperties
+    {
+        public static int STICKER_MAX_COUNT = 60;
+    }
+}
